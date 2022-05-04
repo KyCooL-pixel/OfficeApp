34597e19b7ec98d08d288cd60fac9a24f30fdbce
KyCooL-pixel <kycheok2007@gmail.com>
2022-05-04 13:34:07 +0800

    version 1.0 ( pending for approve )

--- README.md
+++ README.md
@@ -3,16 +3,22 @@
 2) Gui
 
 # The function of the OfficeApp currently have:
-1) Search staff by name, display info about the staff
-2) manually add staff (default staff now is level 2 educator)
+1) Search staff by name / matric number and view info
+2) Add staff
+3) Delete existing staff
+4) Toggle between light and dark theme (for homepage)
 
 # The OfficeApp consist of 4 parts:
 1) The staff package that describe the behaviour of staff and its subclasses
-2) Gui package (not yet packed ) that handles all Gui related events
+2) Gui package that handles all Gui related events
 3) Logic package that handle business logic behind Gui
-4) App to load data ( under development ) then launches Gui and Logic
+4) App that launches Gui and Logic
 
 # Instructions to use the OfficeApp:
-1)Simply run the App.java
+1) Make sure the whole OfficeApp package is downloaded completely
+2) Make sure the r (resources folder) is valid, or images might not be able to load
+3) Run the App.java file
 
-# Note : some functions are filled with dummy methods or objects for development/testing purpose
+# Note 
+for demo purposes, some dummy staffs are initiated inside logic class
+User is recommended to use the default window size (do not maximize or adjust the size manually) to have the best user experience
--- src/Gui/HomePanel.java
+++ src/Gui/HomePanel.java
@@ -1,6 +1,10 @@
 package Gui;
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.Border;
+
+import Logic.Logic;
+
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import java.io.File;
@@ -8,39 +12,131 @@ import java.io.IOException;
 
 public class HomePanel extends JPanel {
     JTextArea text1;
-    JButton testButton;
+    static JButton testButton;
+    JLabel totalstaffLabel;
+    static JPanel topPanel;
+    static JLabel totalstaff;
+    static JLabel moonlabel;
+    static JLabel sunlabel;
+    Font font = new Font("Serif", Font.CENTER_BASELINE, 30);
+    Border border = BorderFactory.createLineBorder(Color.DARK_GRAY, 3);
+
+    static boolean DarkOn = false;
+
     public HomePanel(){
         super();
+        //set homepanel layout
         BorderLayout layout = new BorderLayout();
         setLayout(layout);
+       
+        //declare panel
+        topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        topPanel.setBackground(Color.WHITE);
 
-        testButton =new JButton("Press me to test");
-        add(testButton,BorderLayout.SOUTH);
-
-        JPanel topPanel = new JPanel();
-        BoxLayout topLayout = new BoxLayout(topPanel, BoxLayout.PAGE_AXIS);
-        topPanel.setLayout(topLayout);
+        // init and add pic
         try {
-            BufferedImage officepic = ImageIO.read(new File("D:/My_Projects/OfficeApp/r/office.png"));
-            JLabel piclabel = new JLabel(new ImageIcon(officepic.getScaledInstance(200, 200, Image.SCALE_FAST)));
-            piclabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
+            // load pic
+            BufferedImage moonpic = ImageIO.read(new File("D:/My_Projects/OfficeApp/r/moon.png"));
+            moonlabel = new JLabel(new ImageIcon(moonpic.getScaledInstance(50, 50, Image.SCALE_FAST)));
+            BufferedImage officepic = ImageIO.read(new File("D:/My_Projects/OfficeApp/r/office.png"));
+            JLabel piclabel = new JLabel(new ImageIcon(officepic.getScaledInstance(300, 300, Image.SCALE_FAST)));
+            BufferedImage sunpic = ImageIO.read(new File("D:/My_Projects/OfficeApp/r/sun.png"));
+            sunlabel = new JLabel(new ImageIcon(sunpic.getScaledInstance(50, 50, Image.SCALE_FAST)));
+            
+            // Alignment
+            piclabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+            sunlabel.setAlignmentX(Component.RIGHT_ALIGNMENT);
+            moonlabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+            sunlabel.setAlignmentY(Component.TOP_ALIGNMENT);
+            moonlabel.setAlignmentY(Component.TOP_ALIGNMENT);
+            moonlabel.setVisible(DarkOn);
+            sunlabel.setVisible(!DarkOn);
+
+            // add pic to topPanel
+            topPanel.add(moonlabel);
             topPanel.add(piclabel);
+            topPanel.add(sunlabel);
+
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
-        JLabel text = new JLabel("OfficeApp by Team2");
-        text.setAlignmentX(Component.CENTER_ALIGNMENT);
-        topPanel.add(Box.createRigidArea(new Dimension(0,30)));
-        topPanel.add(text);
         add(topPanel,BorderLayout.NORTH);
        
 
+        // Mid Panel
+        JPanel midpanel = new JPanel();
+        BoxLayout midLayout = new BoxLayout(midpanel, BoxLayout.PAGE_AXIS);
+        midpanel.setLayout(midLayout);
+
+        JLabel text = new JLabel("OfficeApp by Team2");
+        text.setFont(font);
+        text.setAlignmentX(Component.CENTER_ALIGNMENT);
+        midpanel.add(Box.createRigidArea(new Dimension(0,20)));
+        midpanel.add(text);
+        totalstaffLabel = new JLabel("Total staff");
+        
+        totalstaff = new JLabel(" "+(Logic.getTotal())+" ");
+        totalstaff.setFont(font);
+        totalstaff.setBorder(border);
+
+        totalstaffLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        totalstaff.setAlignmentX(Component.CENTER_ALIGNMENT);
+        midpanel.add(Box.createRigidArea(new Dimension(0,20)));
+        midpanel.add(totalstaffLabel);
+        midpanel.add(Box.createRigidArea(new Dimension(0,5)));
+        midpanel.add(totalstaff);
+        
+
+        // Botpanel for darkmode button (named as test button in gui, as im lazy to change)
+        JPanel botpanel = new JPanel(new FlowLayout(FlowLayout.TRAILING));
+
+        try {
+            BufferedImage refreshpic = ImageIO.read(new File("D:/My_Projects/OfficeApp/r/dark.png"));
+            testButton= new JButton(new ImageIcon(refreshpic.getScaledInstance(35, 35, Image.SCALE_FAST)));
+            testButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+            testButton.setBackground(Color.WHITE);
+
+            botpanel.add(testButton);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        botpanel.add(testButton,BorderLayout.SOUTH);
+
+        add(midpanel,BorderLayout.CENTER);
+        add(botpanel,BorderLayout.SOUTH);
     }
 
     public JButton getTestButton(){
         return testButton;
+    }
+
+    // Called every time when add/delete action success
+    public static void refresh(){
+        totalstaff.setText(" "+Logic.getTotal()+" ");
+    }
+
+
+    // DarkMode On/off
+    public static void OnOffTheme(){
+        if(DarkOn){
+            testButton.setBackground(Color.GRAY);
+            topPanel.setBackground(Color.GRAY);
+        }
+        else{
+            testButton.setBackground(Color.WHITE);
+            topPanel.setBackground(Color.WHITE);
+        }
+        moonlabel.setVisible(DarkOn);
+        sunlabel.setVisible(!DarkOn);
+    }
+
+    // Flip boolean for DarkOn
+    public static void flip() {
+        DarkOn = !DarkOn;
     }
 }
